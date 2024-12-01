ff109190b0fd8104022605e30488dcb4cf500f71
Madhav Mundhra <madhavmundhra221@gmail.com>
2024-12-02 03:12:11 +0530

    added frontend with login and register pages

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom';
+import Start from './pages/Start';
+import UserLogin from './pages/UserLogin';
+import UserSignUp from './pages/UserSignUp';
+import CaptainLogin from './pages/CaptainLogin';
+import CaptainSignup from './pages/CaptainSignup';
+import Home from './pages/Home';
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={ <Start /> }/>
+        <Route path='/login' element={ <UserLogin /> }/>
+        <Route path='/signup' element={ <UserSignUp /> }/>
+        <Route path='/captain-login' element={ <CaptainLogin /> }/>
+        <Route path='/captain-signup' element={ <CaptainSignup /> }/>
+        <Route path='/home' element={ <Home />}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App;
