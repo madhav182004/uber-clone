26f0e002933a1cc931e9b2a0266c71b760571dd1
Madhav Mundhra <madhavmundhra221@gmail.com>
2024-12-02 06:17:08 +0530

    Integrated User and Captain Login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,10 @@ import UserSignUp from './pages/UserSignUp';
 import CaptainLogin from './pages/CaptainLogin';
 import CaptainSignup from './pages/CaptainSignup';
 import Home from './pages/Home';
+import UserProtectWrapper from './pages/UserProtectWrapper';
+import UserLogout from './pages/UserLogout';
+import CaptainHome from './pages/CaptainHome';
+import CaptainProtectWrapper from './pages/CaptainProtectWrapper';
 
 const App = () => {
   return (
@@ -16,7 +20,24 @@ const App = () => {
         <Route path='/signup' element={ <UserSignUp /> }/>
         <Route path='/captain-login' element={ <CaptainLogin /> }/>
         <Route path='/captain-signup' element={ <CaptainSignup /> }/>
-        <Route path='/home' element={ <Home />}/>
+        <Route path='/home' 
+          element={ 
+            <UserProtectWrapper>
+              <Home />
+            </UserProtectWrapper>
+        }/>
+        <Route path='/user/logout' 
+          element={ 
+            <UserProtectWrapper>
+              <UserLogout />
+            </UserProtectWrapper>
+        }/>
+        <Route path='/captain-home' 
+          element={ 
+            <CaptainProtectWrapper>
+              <CaptainHome /> 
+            </CaptainProtectWrapper>
+        }/>
       </Routes>
     </div>
   )
